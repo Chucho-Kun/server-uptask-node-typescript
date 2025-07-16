a71a583afbb8260afc1c84787ece2b65660aa981
Jesus Peralta <jesus_web_master@hotmail.com>
2025-07-16 17:22:24 -0600

    cors option b

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -5,8 +5,7 @@ export const corsConfig : CorsOptions = {
     origin: function( origin , callback ){
         console.log({origin});
         const whitelist = [process.env.FRONTEND_URL ]
-        //if( !origin || whitelist.includes(origin)){
-        if(origin === process.env.FRONTEND_URL || !origin){  
+        if( !origin || whitelist.includes(origin)){
         callback(null , true)
         }else{
             callback(new Error('Conexión bloqueada por configuracion de CORS'))
