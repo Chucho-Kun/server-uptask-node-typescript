8176f6b8f67884eba15baac67ce94419317a5ce8
Jesus Peralta <jesus_web_master@hotmail.com>
2025-07-17 12:38:35 -0600

    sending emails

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,6 +1,9 @@
 import type { Request , Response } from "express"
 import User from "../models/User"
 import { hashPassword } from "../utils/auth"
+import Token from "../models/Token"
+import { generateToken } from "../utils/token"
+import { transporter } from "../config/nodemailer"
 
 export class AuthController {
 
@@ -15,12 +18,33 @@ export class AuthController {
                 res.status(409).json({error: error.message})
             }
 
-            //
+            // Crear usuario
             const user = new User( req.body )
 
             // Hash Password
             user.password = await hashPassword( password )
-            await user.save()
+            
+            // Generar token
+            const token = new Token()
+            token.token = generateToken()
+            token.user = user.id
+
+            // enviar el email
+            await transporter.sendMail({
+                from: 'UpTask <admin@uptask.com>',
+                to: user.email,
+                subject: 'UpTask - Confirmar Cuenta',
+                text: 'Puedes confirmar tu cuenta de UpTask en este email',
+                html: `<p style="text-align: center;
+                        font-weight: bold;
+                        background-color: darkblue;
+                        color: white;
+                        padding: 15px 0px;
+                        border-radius: 8px;
+                        font-family: sans-serif;">${ user.name } Confirma tu registro</p>`
+            })
+            
+            await Promise.allSettled([ user.save() , token.save() ])
 
             res.send('Cuenta creada, revisa tu email como confirmarla')
         } catch (error) {
