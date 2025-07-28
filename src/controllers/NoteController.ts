e2b8a0bd6dfd2faa7ed555ad46bd9f6a5458e6c4
Jesus Peralta <jesus_web_master@hotmail.com>
2025-07-28 12:10:26 -0600

    project note managment

--- src/controllers/NoteController.ts
+++ src/controllers/NoteController.ts
@@ -1,5 +1,10 @@
 import { Request , Response } from "express"
 import Note, { INote } from "../models/Note";
+import { Types } from "mongoose";
+
+type NoteParams = {
+    noteId: Types.ObjectId
+}
 
 export class NoteController {
     
@@ -15,6 +20,37 @@ export class NoteController {
         try {
             await Promise.allSettled([req.task.save(), note.save()])
             res.send('Nota creada correctamente')
+        } catch (error) {
+            res.status(500).json({error:'Hubo un error'})
+        }
+    }
+
+    static getTaskNotes = async(req:Request , res:Response) => {
+       try {
+        const notes = await Note.find({task: req.task.id})
+        res.json(notes)
+       } catch (error) {
+        res.status(500).json({error:'Hubo un error'})
+       }
+    }
+
+    static deleteNote = async (req:Request<NoteParams> , res:Response) => {
+        const { noteId } = req.params
+        const note = await Note.findById(noteId)
+
+        if(!note){
+            return res.status(404).json({error:'Nota no encontrada'})
+            //if(!note) return res.status(404).json({error:'Nota no encontrada'})
+        }
+        if(note.createdBy.toString() !== req.user.id.toString() ){
+            return res.status(409).json({error:'Acción no válida'})
+        }
+
+        req.task.notes = req.task.notes.filter(note => note.toString() !== noteId.toString())
+
+        try {
+            await Promise.allSettled([req.task.save(), note.deleteOne()])
+            res.send('Nota eliminada')
         } catch (error) {
             res.status(500).json({error:'Hubo un error'})
         }
--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -135,4 +135,13 @@ router.post('/:projectId/tasks/:taskId/notes',
     NoteController.createNote
 )
 
+router.get('/:projectId/tasks/:taskId/notes',
+    NoteController.getTaskNotes
+)
+
+router.delete('/:projectId/tasks/:taskId/notes/:noteId',
+    param('noteId').isMongoId().withMessage('Id no válido'),
+    handleInputErrors,
+    NoteController.deleteNote
+)
 export default router
